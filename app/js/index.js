bdaf79868a1bb0449c1d97a661f054754b9d9caf
Allan Jiang <origami789@gmail.com>
2020-04-10 01:36:28 -0700

    fix link

--- app/js/index.js
+++ app/js/index.js
@@ -27,7 +27,7 @@ var player = new musical_reveal.Player({
   audio: {
     // src: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'
     // src: 'http://www.hubharp.com/web_sound/BachGavotteShort.mp3'
-    src: '/static/1456.mp3'
+    src: 'static/1456.mp3'
   },
   revealConfig: {
     // parallaxBackgroundImage: 'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg'
